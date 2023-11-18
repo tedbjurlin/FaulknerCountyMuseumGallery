333de61c79757b30f00373bb62071139beefc563
JCT03 <119342826+JCT03@users.noreply.github.com>
2023-11-18 01:30:02 -0600

    Added artwork fields
    Added artwork fields, changed artwork index page to only show basic info, seeded database

--- Pages/Artworks/Edit.cshtml.cs
+++ Pages/Artworks/Edit.cshtml.cs
@@ -71,8 +71,11 @@ namespace FaulknerCountyMuseumGallery.Pages.Artworks
                 s => s.MediumID,
                 s => s.CollectionID,
                 s => s.Title,
+                s => s.AccessionNumber,
                 s => s.ImageLink,
-                s => s.Size))
+                s => s.Size,
+                s => s.Status,
+                s => s.Donor))
             {
                 await _context.SaveChangesAsync();
                 return RedirectToPage("./Index");
