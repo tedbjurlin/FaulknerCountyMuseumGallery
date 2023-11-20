9fd546a6618c287f39783d4b049cbb3a97aabd0e
scottdaco <cooneysd@hendrix.edu>
2023-11-20 12:26:01 -0600

    image loading fix attempt 1

--- wwwroot/js/gallery.js
+++ wwwroot/js/gallery.js
@@ -6,21 +6,19 @@ const handelImageLoad = () => {
     image.removeEventListener('load',handelImageLoad)
 }
 
-{/* <img class=\"M_logo_key_img\" src=\"~/Falkner_county_logo_key.png\"> */}
+// {/* <img class=\"M_logo_key_img\" src=\"~/Falkner_county_logo_key.png\"> */}
 const loader = "<div class=\"loader\"><div></div>";
 const image = document.getElementsByClassName('gallery_image');
 const imagel = image;
 if(!image.complete) {
     for(var i = 0; i < image.length; i++) {
         image[i].addEventListener('load', handelImageLoad);
-        // image[i].src = "";
-        // image[i].alt = "";
         image[i].outerHTML = loader;
     }
 }
 else {
     for(var i = 0; i < image.length; i++) { 
-        image[i] = imagel[i];
+        image[i].outerHTML = imagel[i];
     }
 }
 
